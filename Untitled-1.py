82d5c8f8cb516932aafe4ae7870ea6b1d4ffaa80
Sarai Ramirez <saraiaudivett@gmail.com>
2025-11-19 23:15:54 -0500

    Primer commit: script de saludo en Python

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+
+# hola.py
+# Autor: Sarai Ramirez
+# Descripción: Script básico que saluda al usuario
+
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenido(a) a tu primer proyecto con Git y Python.")
