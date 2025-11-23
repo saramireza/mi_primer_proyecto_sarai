04e0b5d460f06d1bd6c6006e78c04eb6e3150e17
Sarai Ramirez <saraiaudivett@gmail.com>
2025-11-23 18:01:22 -0500

    Primer commit

--- hola.py
+++
@@ -1,6 +0,0 @@
-# hola.py
-# Versión mejorada
-
-nombre = input("¿Cómo te llamas? ")
-print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
-print(f"Tu nombre tiene {len(nombre)} letras. ")
