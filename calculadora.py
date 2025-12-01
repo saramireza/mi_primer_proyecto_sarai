0d62ff1935b3b21402f38f29a920cdfcff4d5572
Sarai <saraiaudivett@gmail.com>
2025-11-30 20:56:45 -0500

    Primer commit: scripts iniciales hola.py y calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+# calculadora.py
+# Script básico para operaciones matemáticas
+numero_1 = float(input("Primer número: ")) 
+numero_2 = float(input("Segundo número: ")) 
+operacion = input("Operación (+, -, *, /): ") 
+
+if operacion == '+': 
+    print("Resultado:", numero_1 + numero_2) 
+elif operacion == '-': 
+    print("Resultado:", numero_1 - numero_2) 
+elif operacion == '*': 
+    print("Resultado:", numero_1 * numero_2) 
+elif operacion == '/':
+    if numero_2 != 0:
+        print("Resultado:", numero_1 / numero_2)
+    else:
+        print("Error: no se puede dividir por cero.") 
+else: 
+    print("Operación no válida.") 
